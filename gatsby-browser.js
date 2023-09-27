3a1cd1c884ac79a00dfd51009ad2459465613b3d
Deadintegral <Deadintegral@gmail.com>
2023-09-27 21:36:59 +0900

    refactor: gatsby5 migration

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import "@fontsource-variable/montserrat"
+import "@fontsource/merriweather"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
